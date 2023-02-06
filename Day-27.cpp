3d5696cf26abb83cfc711fdf7dcd6e457b8066cd
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-02-06 08:44:18 +0530

    leetcode

---
+++ Day-27.cpp
@@ -0,0 +1,18 @@
+// 1470. Shuffle the Array
+
+
+// class Solution {
+// public:
+//     vector<int> shuffle(vector<int>& nums, int n) {
+        
+//         vector<int>ans;
+
+//         for(int i = 0 , j = n; j < nums.size(); i++ , j++)
+//         {
+//             ans.push_back(nums[i]);
+//             ans.push_back(nums[j]);
+//         }
+
+//         return ans;
+//     }
+// };
