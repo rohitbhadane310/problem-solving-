b31c849bd481c5e3ad95e6fa58a72aed4a7558a2
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-02-13 20:06:36 +0530

    leetcode

---
+++ Day-32.cpp
@@ -0,0 +1,21 @@
+// 1523. Count Odd Numbers in an Interval Range
+
+// class Solution {
+// public:
+//     int countOdds(int low, int high) {
+    
+//         int count = 0;
+
+//         for(int i = low; i <= high; i++)
+//         {
+//             int val = i;
+
+//             if(val % 2 != 0)
+//             {
+//                 count++;
+//             }
+//         }
+
+//         return count;
+//     }
+// };
