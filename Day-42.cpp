cd47e269ec7f0f67109a218089b31652826dbdc1
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-02-23 18:40:48 +0530

    leetcode

---
+++ Day-42.cpp
@@ -0,0 +1,39 @@
+// 502. IPO
+
+// class Solution {
+// public:
+//     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        
+//         int n = profits.size();
+//         vector<pair<int , int>>projects;
+
+//         for(int i = 0; i < n; i++)
+//         {
+//             projects.push_back({capital[i] , profits[i]});
+//         }
+
+//         sort(projects.begin() , projects.end());
+//         int i = 0;
+//         priority_queue<int>pq;
+
+//         while(k--)
+//         {
+//             while(i < n && projects[i].first <= w)
+//             {
+//                 pq.push(projects[i].second);
+//                 i++;
+//             }
+//             if(pq.empty())
+//             {
+//                 break;
+//             }
+//             else
+//             {
+//                 w += pq.top();
+//                 pq.pop();
+//             }
+//         }
+
+//         return w;
+//     }
+// };
