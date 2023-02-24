5ba86954f98308f35afbdcaee67323180803f257
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-02-24 13:01:55 +0530

    leetcode

--- Day-43.cpp
+++ Day-43.cpp
@@ -60,3 +60,26 @@
 //         return l3->next;
 //     }
 // };
+
+
+
+// 237. Delete Node in a Linked List
+
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     void deleteNode(ListNode* node) {
+        
+//         ListNode *temp = node->next;
+//         node->val = temp->val;
+
+//         node->next = node->next->next;
+//     }
+// };
