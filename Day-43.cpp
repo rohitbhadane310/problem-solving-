a573b3d2f5b99a142bdb644946292fc090fbd21a
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-02-24 16:34:12 +0530

    leetcode

--- Day-43.cpp
+++ Day-43.cpp
@@ -83,3 +83,40 @@
 //         node->next = node->next->next;
 //     }
 // };
+
+
+// 160. Intersection of Two Linked Lists
+
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+
+//         while(headA)
+//         {
+//             ListNode *temp = headB;
+
+//             while(temp)
+//             {
+//                 if(headA == temp)
+//                 {
+//                     return headA;
+//                 }
+
+//                 temp = temp->next;
+//             }
+
+//             headA = headA->next;
+//         }
+
+//         return NULL;
+//     }
+// };
