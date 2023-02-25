784eeb7b5535d7d7ffc076ef893354863d8ac733
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-02-25 17:53:56 +0530

    leetcode

--- Day-44.cpp
+++ Day-44.cpp
@@ -60,3 +60,40 @@
 //         return true;
 //     }
 // };
+
+
+// 142. Linked List Cycle II
+
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+        
+//         unordered_map<ListNode * , int>mp;
+
+//         ListNode *temp = head;
+
+//         while(temp)
+//         {
+//             if(mp.find(temp) != mp.end())
+//             {
+//                 return temp;
+//             }
+//             else
+//             {
+//                 mp[temp]++;
+//             }
+
+//             temp = temp->next;
+//         }
+
+//         return NULL;
+//     }
+// };
