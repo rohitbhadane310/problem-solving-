c938c4b4121099f25c29d43545fcb6a30d1dcf28
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-03-01 21:06:38 +0530

    leetcode

--- Day-45.cpp
+++ Day-45.cpp
@@ -137,3 +137,28 @@
 //         return ans;
 //     }
 // };
+
+
+// class Solution {
+// public:
+//     int findMaxConsecutiveOnes(vector<int>& nums) {
+        
+//         int ans = 0 , count = 0;
+
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             if(nums[i] == 1)
+//             {
+//                 count++;
+//             }
+//             else
+//             {
+//                 count = 0;
+//             }
+
+//             ans = max(count , ans);
+//         }
+
+//         return ans;
+//     }
+// };
