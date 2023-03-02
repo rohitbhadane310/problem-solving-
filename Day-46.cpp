929da7d6ddd43da11cc1b271c0e0bf32ca3a0acd
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-03-02 20:40:19 +0530

    leetcode

--- Day-46.cpp
+++ Day-46.cpp
@@ -41,3 +41,38 @@
 //         return ans.size();
 //     }
 // };
+
+
+// class Solution {
+// public:
+//     int compress(vector<char>& chars) {
+        
+//         if(chars.size() == 1)
+//         {
+//             return 1;
+//         }
+
+//         int j = 0 , count = 1;
+
+//         for(int i = 1; i <= chars.size(); i++)
+//         {
+//             count = 1;
+//             while(i < chars.size() && chars[i] == chars[i-1])
+//             {
+//                 count++;
+//                 i++;
+//             }
+//             chars[j++] = chars[i-1];
+//             if(count > 1)
+//             {
+//                 string c = to_string(count);
+//                 for(auto ch: c)
+//                 {
+//                     chars[j++] = (char)ch;
+//                 }
+//             }
+//         }
+
+//         return j;
+//     }
+// };
