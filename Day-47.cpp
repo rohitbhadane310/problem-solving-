f96fe77bb2031f67553d87936507a2ae27ffa627
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-03-03 08:53:07 +0530

    leetcode

---
+++ Day-47.cpp
@@ -0,0 +1,16 @@
+// 28. Find the Index of the First Occurrence in a String
+
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+        
+//         int ind = haystack.find(needle);
+
+//         if(ind > -1)
+//         {
+//             return ind;
+//         }
+        
+//         return -1;
+//     }
+// };
