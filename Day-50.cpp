d1a84a70e6ab4b2f19914eceb271b270a50bda3d
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-03-08 21:48:28 +0530

    leetcode

---
+++ Day-50.cpp
@@ -0,0 +1,39 @@
+// class Solution {
+// public:
+//     long long timefunc(int mid , vector<int>&piles)
+//     {
+//         long long sum = 0;
+
+//         for(int i = 0; i< piles.size(); i++)
+//         {
+//             sum += (piles[i] / mid) +((piles[i] % mid) != 0);
+//         }
+
+//         return sum;
+//     }
+//     int minEatingSpeed(vector<int>& piles, int h) {
+        
+//         int low = 1;
+//         int high = *max_element(piles.begin() , piles.end());
+
+//         int ans = 0;
+
+//         while(low <= high)
+//         {
+//             int mid = (low + high) / 2;
+//             int time = timefunc(mid , piles);
+
+//             if(time <= h)
+//             {
+//                 ans = mid;
+//                 high = mid -1;
+//             }
+//             else
+//             {
+//                 low = mid + 1;
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
