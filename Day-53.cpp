1c4e03f108c953980bb14f5db59ce2e12ec1b935
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-03-11 16:06:10 +0530

    leetcode

--- Day-53.cpp
+++ Day-53.cpp
@@ -36,3 +36,46 @@
 //         return ans;
 //     }
 // };
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+     
+//     void solve(int ind , vector<vector<int>>&ans , vector<int>ds , vector<int>arr , int tar)
+//     {
+//         if(ind == arr.size())
+//         {
+//             if(tar == 0)
+//             {
+//                 ans.push_back(ds);
+//             }
+//             return;
+//         }
+
+//         if(arr[ind] <= tar)
+//         {
+//             ds.push_back(arr[ind]);
+//             solve(ind , ans , ds , arr , tar-arr[ind]);
+//             ds.pop_back();
+//         }
+
+//         solve(ind+1 , ans , ds , arr , tar);
+//     }
+
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+//         vector<vector<int>>ans;
+//         vector<int>ds;
+
+//         solve(0 , ans , ds , candidates , target);
+
+//         return ans;
+//     }
+// };
